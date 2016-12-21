ae62e0c40633828df2c4e72e4cc3e071975f53c4
Bijal Soni <bijalsoni@Bijals-MacBook-Air.local>
2016-12-21 18:26:10 +0400

    added book scaffold and homepage logic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161220073203) do
+ActiveRecord::Schema.define(version: 20161221141610) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "name"
+    t.string   "author"
+    t.text     "description"
+    t.integer  "price"
+    t.boolean  "availablity", default: true
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
