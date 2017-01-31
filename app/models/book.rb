d2a5740d32821195eebfcdc610a2e0903bdefef2
Vishay Bhatia <vishaybhatia@addictivegifts.com>
2017-01-31 18:31:47 +0400

    made the first sale

--- app/models/book.rb
+++ app/models/book.rb
@@ -16,4 +16,8 @@ class Book < ApplicationRecord
   content_type: { content_type: "application/pdf" },
   message: 'Only PDF allowed'
 
+  validates_numericality_of :price,
+  greater_than: 49, message: "Price must be at least 50 cents"
+
+
 end
